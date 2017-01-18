7499fb582aa51af9fa6cf7b157aa68edf3a17763
Gael Chamoulaud <gchamoul@redhat.com>
2017-01-18 11:07:45 +0100

    Fix ansible-lint.sh script for roles.
    - According to the ansible-lint documentation, we can pass a role directory as
      an argument. But due to https://github.com/willthames/ansible-lint/issues/210,
      the role directory has to contain a trailing slash!
    
    - This patch fixes the following ansible-lint rules:
      * ANSIBLE0004: Git checkouts must contain explicit version
      * ANSIBLE0008: Deprecated sudo
      * ANSIBLE0011: All tasks should be named
      * ANSIBLE0014: Environment variables don't work as part of command
      * ANSIBLE0015: Using bare variables is deprecated
    
    - Temporarily skipping the following ansible-lint rules (Fixes will be part of
      others patches):
      * ANSIBLE0007: Using command rather than an argument to e.g file
        We have a lot of 'rm' command and we should use file module instead
      * ANSIBLE0012: Commands should not change things if nothing needs doing
      * ANSIBLE0013: Use Shell only when shell functionality is required
    
    Change-Id: I3d5591ea9fd1691e8115d7093db87781a08a6082
    Signed-off-by: Gael Chamoulaud <gchamoul@redhat.com>

--- ci-scripts/ansible-lint.sh
+++ ci-scripts/ansible-lint.sh
@@ -2,19 +2,40 @@
 
 # ANSIBLE0006: Using command rather than module
 #   we have a few use cases where we need to use curl and rsync
+# ANSIBLE0007: Using command rather than an argument to e.g file
+#   we have a lot of 'rm' command and we should use file module instead
+# ANSIBLE0010: Package installs should not use latest.
+#   Sometimes we need to update some packages.
+# ANSIBLE0012: Commands should not change things if nothing needs doing
+# ANSIBLE0013: Use Shell only when shell functionality is required
 # ANSIBLE0016: Tasks that run when changed should likely be handlers
 #   this requires refactoring roles, skipping for now
-SKIPLIST="ANSIBLE0006,ANSIBLE0016"
+SKIPLIST="ANSIBLE0006,ANSIBLE0007,ANSIBLE0010,ANSIBLE0012,ANSIBLE0013,ANSIBLE0016"
+
+function lint_error_check {
+    local rc=$?
+    lint_error=0
+    [ $rc -eq 2 ] && lint_error=1
+    return $lint_error
+}
 
 # lint the playbooks separately to avoid linting the roles multiple times
 pushd playbooks
-find . -type f -regex '.*\.y[a]?ml' -print0 | xargs -0 ansible-lint -x $SKIPLIST || lint_error=1
+for playbook in `find . -type f -regex '.*\.y[a]?ml' -print0`; do
+    ansible-lint -x $SKIPLIST $playbook
+    lint_error_check
+done
 popd
 
 # lint all the possible roles
-find ./roles -type d -print0 | xargs -0 ansible-lint -x $SKIPLIST || lint_error=1
+# Due to https://github.com/willthames/ansible-lint/issues/210, the roles
+# directories need to contain a trailing slash at the end of the path.
+for rolesdir in `find ./roles -maxdepth 1 -type d`; do
+    ansible-lint -x $SKIPLIST $rolesdir/
+    lint_error_check
+done
 
-# exit with 1 if we had any error so far
-if [[ -n "$lint_error" ]]; then
+# exit with 1 if we had a least an error or warning.
+if [[ "$lint_error" != 0 ]]; then
     exit 1;
 fi
