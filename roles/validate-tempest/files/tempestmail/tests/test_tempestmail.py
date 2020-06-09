508446450d3c273498b04ec0316edd226f588e9b
Hervé Beraud <hberaud@redhat.com>
2020-06-09 12:18:07 +0200

    Use unittest.mock instead of mock
    The mock third party library was needed for mock support in py2
    runtimes. Since we now only support py36 and later, we can use the
    standard lib unittest.mock module instead.
    
    Change-Id: Ia06fa93973b153a265ddd0cb2538f5bdabf764aa

--- roles/validate-tempest/files/tempestmail/tests/test_tempestmail.py
+++ roles/validate-tempest/files/tempestmail/tests/test_tempestmail.py
@@ -17,7 +17,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 import datetime
-import mock
+from unittest import mock
 import re
 import tempfile
 import unittest
